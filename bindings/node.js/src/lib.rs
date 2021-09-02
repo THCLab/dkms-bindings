b29d4b36816918c6cfe3f9276874f1ce89f2edcb
Edyta Pawlak <edyta.pawlakk@gmail.com>
2021-09-02 16:07:42 +0200

    Improve errors

--- bindings/node.js/src/lib.rs
+++ bindings/node.js/src/lib.rs
@@ -2,7 +2,7 @@ use std::convert::TryInto;
 
 use keri::{
     event_message::parse::message,
-    prefix::{IdentifierPrefix, Prefix, SelfSigningPrefix},
+    prefix::{BasicPrefix, IdentifierPrefix, Prefix, SelfSigningPrefix},
 };
 use napi::{
     CallContext, Env, JsBoolean, JsBuffer, JsNumber, JsObject, JsString, JsUndefined, Property,
@@ -41,14 +41,20 @@ fn finalize_inception(ctx: CallContext) -> JsResult<JsString> {
     let signature = ctx.get::<JsString>(1)?.into_utf8()?.as_str()?.to_owned();
     let mut cfg = Config::new(Some("settings.cfg"));
     // Read / parse config file
-    cfg.read().ok().expect("Error reading the config file");
+    cfg.read().ok().expect("There is no `settings.cfg` file");
 
-    let path_str = cfg
-        .get("db_path")
-        .expect("Missing database path in settings");
-    let icp = message(&icp).unwrap().1.event_message;
-    let signature: SelfSigningPrefix = signature.parse().expect("Can't parse signature");
-    let kel = KEL::finalize_incept(&path_str, &icp, signature).expect("Error while creating kel");
+    let path_str = cfg.get("db_path").ok_or(napi::Error::from_reason(
+        "Missing `db_path` setting in settings.cfg".into(),
+    ))?;
+    let icp = message(&icp)
+        .map_err(|e| napi::Error::from_reason(e.to_string()))?
+        .1
+        .event_message;
+    let signature: SelfSigningPrefix = signature
+        .parse()
+        .map_err(|_e| napi::Error::from_reason("Wrong signature prefix".into()))?;
+    let kel = KEL::finalize_incept(&path_str, &icp, signature)
+        .map_err(|e| napi::Error::from_reason(e.to_string()))?;
     let identifier = kel.get_prefix().to_str();
     ctx.env.create_string(&identifier)
 }
@@ -58,13 +64,16 @@ fn load_controller(ctx: CallContext) -> JsResult<JsUndefined> {
     let prefix = ctx.get::<JsString>(0)?.into_utf8()?.as_str()?.to_owned();
     let mut cfg = Config::new(Some("settings.cfg"));
     // Read / parse config file
-    cfg.read().ok().expect("Error reading the config file");
+    cfg.read()
+        .ok()
+        .ok_or(napi::Error::from_reason("Can't read a config file".into()))?;
 
-    let path_str = cfg
-        .get("db_path")
-        .expect("Missing database path in settings");
+    let path_str = cfg.get("db_path").ok_or(napi::Error::from_reason(
+        "Missing `db_path` setting in settings.cfg".into(),
+    ))?;
     let prefix: IdentifierPrefix = prefix.parse().expect("Can't parse signature");
-    let kel = KEL::load_kel(&path_str, prefix).map_err(|e| napi::Error::from_reason(e.to_string()))?;
+    let kel =
+        KEL::load_kel(&path_str, prefix).map_err(|e| napi::Error::from_reason(e.to_string()))?;
     let mut this: JsObject = ctx.this_unchecked();
     ctx.env.wrap(&mut this, kel)?;
     ctx.env.get_undefined()
@@ -72,14 +81,33 @@ fn load_controller(ctx: CallContext) -> JsResult<JsUndefined> {
 
 #[js_function(2)]
 fn incept(ctx: CallContext) -> JsResult<JsBuffer> {
-    let current = ctx.get::<JsString>(0)?.into_utf8()?.as_str()?.to_owned();
-    let next = ctx.get::<JsString>(1)?.into_utf8()?.as_str()?.to_owned();
+    let current = ctx
+        .get::<JsString>(0)?
+        .into_utf8()
+        .map_err(|_e| napi::Error::from_reason("Missing current key parameter".into()))?
+        .as_str()?
+        .to_owned();
+    let next = ctx
+        .get::<JsString>(1)?
+        .into_utf8()
+        .map_err(|_e| napi::Error::from_reason("Missing next key parameter".into()))?
+        .as_str()?
+        .to_owned();
 
+    let current_key: BasicPrefix = current
+        .parse()
+        .map_err(|_e| napi::Error::from_reason("Wrong current public key prefix".into()))?;
+    let next_key: BasicPrefix = next
+        .parse()
+        .map_err(|_e| napi::Error::from_reason("Wrong next public key prefix".into()))?;
     let pub_keys = PublicKeysConfig {
-        current: vec![current.parse().expect("Can't parse key")],
-        next: vec![next.parse().expect("Can't parse key")],
+        current: vec![current_key],
+        next: vec![next_key],
     };
-    let icp = KEL::incept(&pub_keys).unwrap().serialize().unwrap();
+    let icp = KEL::incept(&pub_keys)
+        .map_err(|e| napi::Error::from_reason(e.to_string()))?
+        .serialize()
+        .map_err(|e| napi::Error::from_reason(e.to_string()))?;
 
     ctx.env.create_buffer_copy(&icp).map(|b| b.into_raw())
 }
@@ -96,8 +124,11 @@ fn get_prefix(ctx: CallContext) -> JsResult<JsString> {
 fn get_kel(ctx: CallContext) -> JsResult<JsString> {
     let this: JsObject = ctx.this_unchecked();
     let kel: &KEL = ctx.env.unwrap(&this)?;
-    let kel_str = match kel.get_kel().unwrap() {
-        Some(kel) => String::from_utf8(kel).unwrap(),
+    let kel_str = match kel
+        .get_kel()
+        .map_err(|e| napi::Error::from_reason(e.to_string()))?
+    {
+        Some(kel) => String::from_utf8(kel).map_err(|e| napi::Error::from_reason(e.to_string()))?,
         None => "".to_owned(),
     };
     ctx.env.create_string(&kel_str)
@@ -107,19 +138,25 @@ fn get_kel(ctx: CallContext) -> JsResult<JsString> {
 fn rotate(ctx: CallContext) -> JsResult<JsBuffer> {
     let new_current = ctx.get::<JsString>(0)?.into_utf8()?.as_str()?.to_owned();
     let new_next = ctx.get::<JsString>(1)?.into_utf8()?.as_str()?.to_owned();
+    let current_key: BasicPrefix = new_current
+        .parse()
+        .map_err(|_e| napi::Error::from_reason("Wrong current public key prefix".into()))?;
+    let next_key: BasicPrefix = new_next
+        .parse()
+        .map_err(|_e| napi::Error::from_reason("Wrong next public key prefix".into()))?;
 
     let pub_keys = PublicKeysConfig {
-        current: vec![new_current.parse().expect("Cn't parse key")],
-        next: vec![new_next.parse().expect("Can't parse key")],
+        current: vec![current_key],
+        next: vec![next_key],
     };
 
     let this: JsObject = ctx.this_unchecked();
     let kel: &mut KEL = ctx.env.unwrap(&this)?;
     let rot_event = kel
         .rotate(&pub_keys)
-        .expect("No rotation event")
+        .map_err(|e| napi::Error::from_reason(e.to_string()))?
         .serialize()
-        .unwrap();
+        .map_err(|e| napi::Error::from_reason(e.to_string()))?;
     ctx.env.create_buffer_copy(&rot_event).map(|b| b.into_raw())
 }
 
@@ -129,7 +166,9 @@ fn finalize_rotation(ctx: CallContext) -> JsResult<JsBoolean> {
     let signature = ctx.get::<JsString>(1)?.into_utf8()?.as_str()?.to_owned();
     let this: JsObject = ctx.this_unchecked();
     let kel: &KEL = ctx.env.unwrap(&this)?;
-    let signature: SelfSigningPrefix = signature.parse().expect("Can't parse signature");
+    let signature: SelfSigningPrefix = signature
+        .parse()
+        .map_err(|_e| napi::Error::from_reason("Wrong signature prefix".into()))?;
     let rot_result = kel.finalize_rotation(rot, signature);
     ctx.env.get_boolean(rot_result.is_ok())
 }
@@ -139,23 +178,27 @@ fn process(ctx: CallContext) -> JsResult<JsUndefined> {
     let stream = ctx.get::<JsBuffer>(0)?.into_value()?.to_vec();
     let this: JsObject = ctx.this_unchecked();
     let kel: &KEL = ctx.env.unwrap(&this)?;
-    kel.process_stream(&stream).expect("Error while processing");
+    kel.process_stream(&stream)
+        .map_err(|e| napi::Error::from_reason(e.to_string()))?;
     ctx.env.get_undefined()
 }
 
 #[js_function(1)]
 fn get_current_public_key(ctx: CallContext) -> JsResult<JsBuffer> {
     let identifier: String = ctx.get::<JsString>(0)?.into_utf8()?.as_str()?.to_owned();
-    let prefix: IdentifierPrefix = identifier.parse().expect("Can't parse prefix");
+    let prefix: IdentifierPrefix = identifier
+        .parse()
+        .map_err(|_e| napi::Error::from_reason("Wrong identifeir prefix".into()))?;
     let this: JsObject = ctx.this_unchecked();
     let kel: &KEL = ctx.env.unwrap(&this)?;
-    // TODO For now assume that there is only one key.
     let key = kel
         .get_current_public_keys(&prefix)
-        .expect("Error while processing")
-        .unwrap()[0]
-        .key
-        .clone();
+        .map_err(|_e| napi::Error::from_reason("Wrong identifeir prefix".into()))?
+        .ok_or(napi::Error::from_reason(format!("There is no keys for prefix {}", identifier)))?
+        // TODO For now assume that there is only one key.
+        [0]
+    .key
+    .clone();
     ctx.env.create_buffer_copy(&key).map(|b| b.into_raw())
 }
 
@@ -166,12 +209,18 @@ fn verify(ctx: CallContext) -> JsResult<JsBoolean> {
     let identifier: String = ctx.get::<JsString>(2)?.into_utf8()?.as_str()?.to_owned();
     let this: JsObject = ctx.this_unchecked();
     let kel: &KEL = ctx.env.unwrap(&this)?;
-    let prefix = identifier.parse().expect("Can't parse prefix");
-    let signature: SelfSigningPrefix = signature.parse().expect("Can't parse signature");
-    ctx.env.get_boolean(
-        kel.verify(&message, &signature, &prefix)
-            .expect("Error while verifing"),
-    )
+    let prefix = identifier
+        .parse()
+        .map_err(|_e| napi::Error::from_reason("Wrong identifeir prefix".into()))?;
+
+    let signature: SelfSigningPrefix = signature
+        .parse()
+        .map_err(|_e| napi::Error::from_reason("Wrong signature prefix".into()))?;
+
+    ctx.env
+        .get_boolean(kel.verify(&message, &signature, &prefix).map_err(|e| {
+            napi::Error::from_reason(format!("Error while verifing: {}", e.to_string()))
+        })?)
 }
 
 #[js_function(4)]
@@ -182,11 +231,17 @@ fn verify_at_sn(ctx: CallContext) -> JsResult<JsBoolean> {
     let sn: i64 = ctx.get::<JsNumber>(3)?.try_into()?;
     let this: JsObject = ctx.this_unchecked();
     let kel: &KEL = ctx.env.unwrap(&this)?;
-    let prefix = identifier.parse().expect("Can't parse prefix");
-    let signature: SelfSigningPrefix = signature.parse().expect("Can't parse signature");
+    let prefix = identifier
+        .parse()
+        .map_err(|_e| napi::Error::from_reason("Wrong identifier prefix".into()))?;
+    let signature: SelfSigningPrefix = signature
+        .parse()
+        .map_err(|_e| napi::Error::from_reason("Wrong signature prefix".into()))?;
     ctx.env.get_boolean(
         kel.verify_at_sn(&message, &signature, &prefix, sn as u64)
-            .expect("Error while verifing"),
+            .map_err(|e| {
+                napi::Error::from_reason(format!("Error while verifing: {}", e.to_string()))
+            })?,
     )
 }
 
@@ -194,6 +249,8 @@ fn verify_at_sn(ctx: CallContext) -> JsResult<JsBoolean> {
 fn get_current_sn(ctx: CallContext) -> JsResult<JsNumber> {
     let this: JsObject = ctx.this_unchecked();
     let kel: &KEL = ctx.env.unwrap(&this)?;
-    let sn = kel.current_sn().expect("Can't get sn");
+    let sn = kel
+        .current_sn()
+        .map_err(|e| napi::Error::from_reason(format!("Can't get sn: {}", e.to_string())))?;
     ctx.env.create_int64(sn as i64)
 }
