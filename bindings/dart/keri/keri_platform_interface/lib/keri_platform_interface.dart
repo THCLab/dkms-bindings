ea9348854c64568f341a44ec773e4da45ca2735f
gredajustyna <83274413+gredajustyna@users.noreply.github.com>
2022-11-22 14:09:11 +0100

    platform interface published

--- bindings/dart/keri/keri_platform_interface/lib/keri_platform_interface.dart
+++ bindings/dart/keri/keri_platform_interface/lib/keri_platform_interface.dart
@@ -3,7 +3,7 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 import 'bridge_generated.dart';
 
-class KeriPlatformInterface extends PlatformInterface{
+class KeriPlatformInterface extends PlatformInterface {
   KeriPlatformInterface() : super(token: _token);
 
   static final Object _token = Object();
@@ -23,61 +23,60 @@ class KeriPlatformInterface extends PlatformInterface{
   ///Initializes database for storing events.
   Future<bool> initKel(
       {required String inputAppDir,
-        Config? optionalConfigs,
-        dynamic hint}) async {
+      Config? optionalConfigs,
+      dynamic hint}) async {
     throw UnimplementedError('initKel() has not been implemented.');
   }
 
   ///Creates inception event that needs to be signed externally.
   Future<String> incept(
       {required List<PublicKey> publicKeys,
-        required List<PublicKey> nextPubKeys,
-        required List<String> witnesses,
-        required int witnessThreshold,
-        dynamic hint}) async {
+      required List<PublicKey> nextPubKeys,
+      required List<String> witnesses,
+      required int witnessThreshold,
+      dynamic hint}) async {
     throw UnimplementedError('incept() has not been implemented.');
   }
 
   ///Finalizes inception (bootstrapping an Identifier and its Key Event Log).
   Future<Identifier?> finalizeInception(
       {required String event,
-        required Signature signature,
-        dynamic hint}) async {
+      required Signature signature,
+      dynamic hint}) async {
     throw UnimplementedError('finalizeInception() has not been implemented.');
   }
 
   ///Creates rotation event that needs to be signed externally.
   Future<String> rotate(
       {required Identifier controller,
-        required List<PublicKey> currentKeys,
-        required List<PublicKey> newNextKeys,
-        required List<String> witnessToAdd,
-        required List<String> witnessToRemove,
-        required int witnessThreshold,
-        dynamic hint}) async {
+      required List<PublicKey> currentKeys,
+      required List<PublicKey> newNextKeys,
+      required List<String> witnessToAdd,
+      required List<String> witnessToRemove,
+      required int witnessThreshold,
+      dynamic hint}) async {
     throw UnimplementedError('rotate() has not been implemented.');
   }
 
   ///Creates new reply message with identifier's watcher. It needs to be signed externally and finalized with finalizeEvent.
   Future<String> addWatcher(
       {required Identifier controller,
-        required String watcherOobi,
-        dynamic hint}) async {
+      required String watcherOobi,
+      dynamic hint}) async {
     throw UnimplementedError('addWatcher() has not been implemented.');
   }
 
   ///Verifies provided signatures against event and saves it.
   Future<bool> finalizeEvent(
       {required Identifier identifier,
-        required String event,
-        required Signature signature,
-        dynamic hint}) async {
+      required String event,
+      required Signature signature,
+      dynamic hint}) async {
     throw UnimplementedError('finalizeEvent() has not been implemented.');
   }
 
   ///Checks and saves provided identifier's endpoint information.
-  Future<bool> resolveOobi(
-      {required String oobiJson, dynamic hint}) async {
+  Future<bool> resolveOobi({required String oobiJson, dynamic hint}) async {
     throw UnimplementedError('resolveOobi() has not been implemented.');
   }
 
@@ -95,17 +94,17 @@ class KeriPlatformInterface extends PlatformInterface{
   ///Creates new Interaction Event along with provided Self Addressing Identifiers.
   Future<String> anchorDigest(
       {required Identifier controller,
-        required List<String> sais,
-        dynamic hint}) async {
+      required List<String> sais,
+      dynamic hint}) async {
     throw UnimplementedError('anchorDigest() has not been implemented.');
   }
 
   ///Creates new Interaction Event along with arbitrary data.
   Future<String> anchor(
       {required Identifier controller,
-        required String data,
-        required DigestType algo,
-        dynamic hint}) async {
+      required String data,
+      required DigestType algo,
+      dynamic hint}) async {
     throw UnimplementedError('anchor() has not been implemented.');
   }
 
@@ -117,43 +116,44 @@ class KeriPlatformInterface extends PlatformInterface{
 
   Future<List<String>> queryMailbox(
       {required Identifier whoAsk,
-        required Identifier aboutWho,
-        required List<String> witness,
-        dynamic hint}) async {
+      required Identifier aboutWho,
+      required List<String> witness,
+      dynamic hint}) async {
     throw UnimplementedError('queryMailbox() has not been implemented.');
   }
 
   Future<List<ActionRequired>> finalizeMailboxQuery(
       {required Identifier identifier,
-        required String queryEvent,
-        required Signature signature,
-        dynamic hint}) async {
-    throw UnimplementedError('finalizeMailboxQuery() has not been implemented.');
+      required String queryEvent,
+      required Signature signature,
+      dynamic hint}) async {
+    throw UnimplementedError(
+        'finalizeMailboxQuery() has not been implemented.');
   }
 
   Future<Signature?> signatureFromHex(
       {required SignatureType st,
-        required String signature,
-        dynamic hint}) async {
+      required String signature,
+      dynamic hint}) async {
     throw UnimplementedError('signatureFromHex() has not been implemented.');
   }
 
   Future<GroupInception?> inceptGroup(
       {required Identifier identifier,
-        required List<Identifier> participants,
-        required int signatureThreshold,
-        required List<String> initialWitnesses,
-        required int witnessThreshold,
-        dynamic hint}) async {
+      required List<Identifier> participants,
+      required int signatureThreshold,
+      required List<String> initialWitnesses,
+      required int witnessThreshold,
+      dynamic hint}) async {
     throw UnimplementedError('inceptGroup() has not been implemented.');
   }
 
   Future<Identifier?> finalizeGroupIncept(
       {required Identifier identifier,
-        required String groupEvent,
-        required Signature signature,
-        required List<DataAndSignature> toForward,
-        dynamic hint}) async {
+      required String groupEvent,
+      required Signature signature,
+      required List<DataAndSignature> toForward,
+      dynamic hint}) async {
     throw UnimplementedError('finalizeGroupIncept() has not been implemented.');
   }
 
@@ -164,8 +164,8 @@ class KeriPlatformInterface extends PlatformInterface{
 
   Future<DataAndSignature?> newDataAndSignature(
       {required String data,
-        required Signature signature,
-        dynamic hint}) async {
+      required Signature signature,
+      dynamic hint}) async {
     throw UnimplementedError('newDataAndSignature() has not been implemented.');
   }
 }
--- bindings/dart/keri/keri_platform_interface/lib/keri_platform_interface_method_channel.dart
+++ bindings/dart/keri/keri_platform_interface/lib/keri_platform_interface_method_channel.dart
@@ -9,65 +9,63 @@ class MethodChannelKeriPlatformInterface extends KeriPlatformInterface {
   @visibleForTesting
   final methodChannel = const MethodChannel('keri_platform_interface');
 
-
   ///Initializes database for storing events.
   Future<bool> initKel(
       {required String inputAppDir,
-        Config? optionalConfigs,
-        dynamic hint}) async {
+      Config? optionalConfigs,
+      dynamic hint}) async {
     return false;
   }
 
   ///Creates inception event that needs to be signed externally.
   Future<String> incept(
       {required List<PublicKey> publicKeys,
-        required List<PublicKey> nextPubKeys,
-        required List<String> witnesses,
-        required int witnessThreshold,
-        dynamic hint}) async {
+      required List<PublicKey> nextPubKeys,
+      required List<String> witnesses,
+      required int witnessThreshold,
+      dynamic hint}) async {
     return 'default string - incept';
   }
 
   ///Finalizes inception (bootstrapping an Identifier and its Key Event Log).
   Future<Identifier?> finalizeInception(
       {required String event,
-        required Signature signature,
-        dynamic hint}) async {
+      required Signature signature,
+      dynamic hint}) async {
     return null;
   }
 
   ///Creates rotation event that needs to be signed externally.
   Future<String> rotate(
       {required Identifier controller,
-        required List<PublicKey> currentKeys,
-        required List<PublicKey> newNextKeys,
-        required List<String> witnessToAdd,
-        required List<String> witnessToRemove,
-        required int witnessThreshold,
-        dynamic hint}) async {
+      required List<PublicKey> currentKeys,
+      required List<PublicKey> newNextKeys,
+      required List<String> witnessToAdd,
+      required List<String> witnessToRemove,
+      required int witnessThreshold,
+      dynamic hint}) async {
     return 'default string - rotate';
   }
 
   ///Creates new reply message with identifier's watcher. It needs to be signed externally and finalized with finalizeEvent.
   Future<String> addWatcher(
       {required Identifier controller,
-        required String watcherOobi,
-        dynamic hint}) async {
+      required String watcherOobi,
+      dynamic hint}) async {
     return 'default string - addWatcher';
   }
 
   ///Verifies provided signatures against event and saves it.
   Future<bool> finalizeEvent(
       {required Identifier identifier,
-        required String event,
-        required Signature signature,
-        dynamic hint}) async {
+      required String event,
+      required Signature signature,
+      dynamic hint}) async {
     return false;
   }
 
   ///Checks and saves provided identifier's endpoint information.
-  Future<bool> resolveOobi(
-      {required String oobiJson, dynamic hint}) async {
+  Future<bool> resolveOobi({required String oobiJson, dynamic hint}) async {
     return false;
   }
 
@@ -85,17 +83,17 @@ class MethodChannelKeriPlatformInterface extends KeriPlatformInterface {
   ///Creates new Interaction Event along with provided Self Addressing Identifiers.
   Future<String> anchorDigest(
       {required Identifier controller,
-        required List<String> sais,
-        dynamic hint}) async {
+      required List<String> sais,
+      dynamic hint}) async {
     return 'default string - anchorDigest';
   }
 
   ///Creates new Interaction Event along with arbitrary data.
   Future<String> anchor(
       {required Identifier controller,
-        required String data,
-        required DigestType algo,
-        dynamic hint}) async {
+      required String data,
+      required DigestType algo,
+      dynamic hint}) async {
     return 'default string - anchor';
   }
 
@@ -107,43 +105,43 @@ class MethodChannelKeriPlatformInterface extends KeriPlatformInterface {
 
   Future<List<String>> queryMailbox(
       {required Identifier whoAsk,
-        required Identifier aboutWho,
-        required List<String> witness,
-        dynamic hint}) async {
+      required Identifier aboutWho,
+      required List<String> witness,
+      dynamic hint}) async {
     return [];
   }
 
   Future<List<ActionRequired>> finalizeMailboxQuery(
       {required Identifier identifier,
-        required String queryEvent,
-        required Signature signature,
-        dynamic hint}) async {
+      required String queryEvent,
+      required Signature signature,
+      dynamic hint}) async {
     return [];
   }
 
   Future<Signature?> signatureFromHex(
       {required SignatureType st,
-        required String signature,
-        dynamic hint}) async {
+      required String signature,
+      dynamic hint}) async {
     return null;
   }
 
   Future<GroupInception?> inceptGroup(
       {required Identifier identifier,
-        required List<Identifier> participants,
-        required int signatureThreshold,
-        required List<String> initialWitnesses,
-        required int witnessThreshold,
-        dynamic hint}) async {
+      required List<Identifier> participants,
+      required int signatureThreshold,
+      required List<String> initialWitnesses,
+      required int witnessThreshold,
+      dynamic hint}) async {
     return null;
   }
 
   Future<Identifier?> finalizeGroupIncept(
       {required Identifier identifier,
-        required String groupEvent,
-        required Signature signature,
-        required List<DataAndSignature> toForward,
-        dynamic hint}) async {
+      required String groupEvent,
+      required Signature signature,
+      required List<DataAndSignature> toForward,
+      dynamic hint}) async {
     return null;
   }
 
@@ -154,9 +152,8 @@ class MethodChannelKeriPlatformInterface extends KeriPlatformInterface {
 
   Future<DataAndSignature?> newDataAndSignature(
       {required String data,
-        required Signature signature,
-        dynamic hint}) async {
+      required Signature signature,
+      dynamic hint}) async {
     return null;
   }
-
 }
