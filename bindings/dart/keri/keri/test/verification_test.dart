feb54b94d03918c1716c99e575c1d34c053f05ed
Edyta Pawlak <edyta.pawlakk@gmail.com>
2023-10-17 17:57:50 +0200

    Fix verification test

--- bindings/dart/keri/keri/test/verification_test.dart
+++ bindings/dart/keri/keri/test/verification_test.dart
@@ -1,79 +1,54 @@
 import 'dart:convert';
-import 'dart:ffi';
 import 'dart:io';
 
 import 'package:flutter_rust_bridge/flutter_rust_bridge.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:keri/keri.dart';
-import 'package:keri_windows/exceptions.dart';
-import 'dart:convert';
 import 'package:convert/convert.dart';
-
-import 'package:path/path.dart';
-//import 'package:test/expect.dart' as ex;
-
 import 'package:cryptography/cryptography.dart';
+import 'package:path/path.dart';
 
 Future<void> main() async {
-// void main() {
-
   final api =
       KeriDartImpl(loadLibForDart("../../target/release/libdartkeriox.so"));
   final algorithm = Ed25519();
-  final keyPair0 = await algorithm.newKeyPair();
-  final keyPair1 = await algorithm.newKeyPair();
-  final b64_current_key =
-      base64Url.encode((await keyPair0.extractPublicKey()).bytes);
-  final b64_next_key =
-      base64Url.encode((await keyPair1.extractPublicKey()).bytes);
+  final signerKeyPair0 = await algorithm.newKeyPair();
+  final signerKeyPair1 = await algorithm.newKeyPair();
+  final b64CurrentKey =
+      base64Url.encode((await signerKeyPair0.extractPublicKey()).bytes);
+  final b64NextKey =
+      base64Url.encode((await signerKeyPair1.extractPublicKey()).bytes);
 
   final verifierKeyPair0 = await algorithm.newKeyPair();
   final verifierKeyPair1 = await algorithm.newKeyPair();
-  final verifier_b64_current_key =
+  final verifierB64CurrentKey =
       base64Url.encode((await verifierKeyPair0.extractPublicKey()).bytes);
-  final verifier_b64_next_key =
+  final verifierB64NextKey =
       base64Url.encode((await verifierKeyPair1.extractPublicKey()).bytes);
 
-  // Generate a key pair
-
-  // Sign a message
-  final message = <int>[1, 2, 3];
-  final signature = await algorithm.sign(
-    message,
-    keyPair: keyPair0,
-  );
-  print('Signature bytes: ${signature.bytes}');
-  print('Public key: ${signature}');
-  print('keypair: ${keyPair0}');
-
-  // Anyone can verify the signature
-  final isSignatureCorrect = await algorithm.verify(
-    message,
-    signature: signature,
-  );
-
-  Future<void> collect_receipts(identifier, witnesses, keypair) async {
-    api.notifyWitnesses(identifier: identifier);
+  // Helper function for publishing KEL events to Witnesses.
+  Future<void> collectReceipts(identifier, witnesses, keyPair) async {
+    await api.notifyWitnesses(identifier: identifier);
 
     var query = await api.queryMailbox(
         whoAsk: identifier, aboutWho: identifier, witness: witnesses);
 
     for (final qry in query) {
-      var raw_signature = await algorithm.sign(
+      var rawSignature = await algorithm.sign(
         utf8.encode(qry),
-        keyPair: keypair,
+        keyPair: keyPair,
       );
-      var hex_signature = await api.signatureFromHex(
+      var hexSignature = await api.signatureFromHex(
           st: SignatureType.Ed25519Sha512,
-          signature: hex.encode(raw_signature.bytes));
+          signature: hex.encode(rawSignature.bytes));
       await api.finalizeQuery(
-          identifier: identifier, queryEvent: qry, signature: hex_signature);
+          identifier: identifier, queryEvent: qry, signature: hexSignature);
     }
   }
 
   test('Test verification', () async {
     final current = await api
-        .newPublicKey(kt: KeyType.Ed25519, keyB64UrlSafe: b64_current_key)
+        .newPublicKey(kt: KeyType.Ed25519, keyB64UrlSafe: b64CurrentKey)
         .catchError((onError) {
       expect(onError.toString(), 'error');
     });
@@ -82,88 +57,82 @@ Future<void> main() async {
       Directory.current.path,
       Directory.current.path.endsWith('test') ? '' : 'test/db1',
     );
-    print(testDirectory);
 
-    var current_keys = [current];
-    print("${current.publicKey}");
+    var currentKeys = [current];
     var next = await api
-        .newPublicKey(kt: KeyType.Ed25519, keyB64UrlSafe: b64_next_key)
+        .newPublicKey(kt: KeyType.Ed25519, keyB64UrlSafe: b64NextKey)
         .catchError((onError) {
       expect(onError.toString(), 'error');
     });
-    var next_keys = [next];
-    print("${next.publicKey}");
-    // var witness_oobi = ['{"eid":"BJq7UABlttINuWJh1Xl2lkqZG4NTdUdqnbFJDa6ZyxCC","scheme":"http","url":"http://witness1.sandbox.argo.colossi.network/"}'];
-    var witness_oobi = [
-      '{"eid":"BJq7UABlttINuWJh1Xl2lkqZG4NTdUdqnbFJDa6ZyxCC","scheme":"http","url":"http://localhost:3232/"}'
+    var nextKeys = [next];
+    var witnessOobi = [
+      '{"eid":"BJq7UABlttINuWJh1Xl2lkqZG4NTdUdqnbFJDa6ZyxCC","scheme":"http","url":"http://witness1.sandbox.argo.colossi.network/"}'
     ];
-    var witness_id = "BJq7UABlttINuWJh1Xl2lkqZG4NTdUdqnbFJDa6ZyxCC";
-    print("${witness_oobi[0]}");
+    // var witnessOobi = [
+    //   '{"eid":"BJq7UABlttINuWJh1Xl2lkqZG4NTdUdqnbFJDa6ZyxCC","scheme":"http","url":"http://localhost:3232/"}'
+    // ];
+    var witnessId = "BJq7UABlttINuWJh1Xl2lkqZG4NTdUdqnbFJDa6ZyxCC";
     await api.initKel(inputAppDir: testDirectory);
 
     // Setup signing identifier
-    var icp_event = await api.incept(
-        publicKeys: current_keys,
-        nextPubKeys: next_keys,
-        witnesses: witness_oobi,
+    var icpEvent = await api.incept(
+        publicKeys: currentKeys,
+        nextPubKeys: nextKeys,
+        witnesses: witnessOobi,
         witnessThreshold: 1);
 
-    var raw_signature = await algorithm.sign(
-      utf8.encode(icp_event),
-      keyPair: keyPair0,
+    var rawSignature = await algorithm.sign(
+      utf8.encode(icpEvent),
+      keyPair: signerKeyPair0,
     );
-    var hex_signature = await api.signatureFromHex(
+    var hexSignature = await api.signatureFromHex(
         st: SignatureType.Ed25519Sha512,
-        signature: hex.encode(raw_signature.bytes));
+        signature: hex.encode(rawSignature.bytes));
 
-    var signing_identifier =
-        await api.finalizeInception(event: icp_event, signature: hex_signature);
+    var signingIdentifier =
+        await api.finalizeInception(event: icpEvent, signature: hexSignature);
 
-    await collect_receipts(signing_identifier, [witness_id], keyPair0);
+    await collectReceipts(signingIdentifier, [witnessId], signerKeyPair0);
 
     // Setup TEL
-    var ixn = await api.inceptRegistry(identifier: signing_identifier);
+    var ixn = await api.inceptRegistry(identifier: signingIdentifier);
 
-    raw_signature = await algorithm.sign(
+    rawSignature = await algorithm.sign(
       utf8.encode(ixn.ixn),
-      keyPair: keyPair0,
+      keyPair: signerKeyPair0,
     );
-    hex_signature = await api.signatureFromHex(
+    hexSignature = await api.signatureFromHex(
         st: SignatureType.Ed25519Sha512,
-        signature: hex.encode(raw_signature.bytes));
+        signature: hex.encode(rawSignature.bytes));
 
     await api.finalizeEvent(
-        identifier: signing_identifier,
-        event: ixn.ixn,
-        signature: hex_signature);
-    await collect_receipts(signing_identifier, [witness_id], keyPair0);
+        identifier: signingIdentifier, event: ixn.ixn, signature: hexSignature);
+    await collectReceipts(signingIdentifier, [witnessId], signerKeyPair0);
 
-    var kel = await api.getKel(identifier: signing_identifier);
-    print("kel: ${kel}");
+    // var kel = await api.getKel(identifier: signingIdentifier);
 
     // Sign message
-    var message = '{"m":"hello there"}';
+    var message = '{"i":"${signingIdentifier.id}","m":"hello there"}';
+    print("\n\nmessage: ${message}");
 
-    raw_signature = await algorithm.sign(
-      utf8.encode(message),
-      keyPair: keyPair0,
+    var credential = await api.issueCredential(
+        identifier: signingIdentifier, credential: message);
+
+    rawSignature = await algorithm.sign(
+      utf8.encode(credential.ixn),
+      keyPair: signerKeyPair0,
     );
-    hex_signature = await api.signatureFromHex(
+    hexSignature = await api.signatureFromHex(
         st: SignatureType.Ed25519Sha512,
-        signature: hex.encode(raw_signature.bytes));
+        signature: hex.encode(rawSignature.bytes));
 
-    var signed = await api.signToCesr(
-        identifier: signing_identifier,
-        data: message,
-        signature: hex_signature);
-    print("Signed: ${signed}");
+    await api.finalizeEvent(
+        identifier: signingIdentifier,
+        event: credential.ixn,
+        signature: hexSignature);
+    await collectReceipts(signingIdentifier, [witnessId], signerKeyPair0);
 
-    // var credential = await api.issueCredential(identifier: signing_identifier, credential: message);
-
-    // await api.finalizeEvent(identifier: signing_identifier, event: ixn.ixn, signature: hex_signature);
-    // await collect_receipts(signing_identifier, [witness_id], keyPair0);
-
-    // await api.notifyBackers(identifier: signing_identifier);
+    await api.notifyBackers(identifier: signingIdentifier);
 
     // Setup verifying identifier
     var verifierDbPath = join(
@@ -173,100 +142,111 @@ Future<void> main() async {
 
     await api.changeController(dbPath: verifierDbPath);
 
-    current_keys = [
+    currentKeys = [
       await api
           .newPublicKey(
-              kt: KeyType.Ed25519, keyB64UrlSafe: verifier_b64_current_key)
+              kt: KeyType.Ed25519, keyB64UrlSafe: verifierB64CurrentKey)
           .catchError((onError) {
         expect(onError.toString(), 'error');
       })
     ];
-    next_keys = [
+    nextKeys = [
       await api
-          .newPublicKey(
-              kt: KeyType.Ed25519, keyB64UrlSafe: verifier_b64_next_key)
+          .newPublicKey(kt: KeyType.Ed25519, keyB64UrlSafe: verifierB64NextKey)
           .catchError((onError) {
         expect(onError.toString(), 'error');
       })
     ];
 
-    icp_event = await api.incept(
-        publicKeys: current_keys,
-        nextPubKeys: next_keys,
-        witnesses: witness_oobi,
+    icpEvent = await api.incept(
+        publicKeys: currentKeys,
+        nextPubKeys: nextKeys,
+        witnesses: witnessOobi,
         witnessThreshold: 1);
 
-    raw_signature = await algorithm.sign(
-      utf8.encode(icp_event),
+    rawSignature = await algorithm.sign(
+      utf8.encode(icpEvent),
       keyPair: verifierKeyPair0,
     );
-    hex_signature = await api.signatureFromHex(
+
+    hexSignature = await api.signatureFromHex(
         st: SignatureType.Ed25519Sha512,
-        signature: hex.encode(raw_signature.bytes));
+        signature: hex.encode(rawSignature.bytes));
 
     var verifyingIdentifier =
-        await api.finalizeInception(event: icp_event, signature: hex_signature);
+        await api.finalizeInception(event: icpEvent, signature: hexSignature);
 
-    await collect_receipts(verifyingIdentifier, [witness_id], verifierKeyPair0);
+    await collectReceipts(verifyingIdentifier, [witnessId], verifierKeyPair0);
 
     // Setup watcher
-    // var watcherOobi = '{"eid":"BF2t2NPc1bwptY1hYV0YCib1JjQ11k9jtuaZemecPF5b","scheme":"http","url":"http://watcher.sandbox.argo.colossi.network/"}';
     var watcherOobi =
-        '{"eid":"BF2t2NPc1bwptY1hYV0YCib1JjQ11k9jtuaZemecPF5b","scheme":"http","url":"http://localhost:3236/"}';
+        '{"eid":"BF2t2NPc1bwptY1hYV0YCib1JjQ11k9jtuaZemecPF5b","scheme":"http","url":"http://watcher.sandbox.argo.colossi.network/"}';
+    // var watcherOobi =
+    //     '{"eid":"BF2t2NPc1bwptY1hYV0YCib1JjQ11k9jtuaZemecPF5b","scheme":"http","url":"http://localhost:3236/"}';
     var add_watcher = await api.addWatcher(
         identifier: verifyingIdentifier, watcherOobi: watcherOobi);
-    print("\n add watcher: ${add_watcher}");
 
-    raw_signature = await algorithm.sign(
+    rawSignature = await algorithm.sign(
       utf8.encode(add_watcher),
       keyPair: verifierKeyPair0,
     );
-    hex_signature = await api.signatureFromHex(
+    hexSignature = await api.signatureFromHex(
         st: SignatureType.Ed25519Sha512,
-        signature: hex.encode(raw_signature.bytes));
+        signature: hex.encode(rawSignature.bytes));
 
     await api.finalizeEvent(
         identifier: verifyingIdentifier,
         event: add_watcher,
-        signature: hex_signature);
+        signature: hexSignature);
 
     var issuer_oobi =
-        '{"cid":"${signing_identifier.id}","role":"witness","eid":"BJq7UABlttINuWJh1Xl2lkqZG4NTdUdqnbFJDa6ZyxCC"}';
+        '{"cid":"${signingIdentifier.id}","role":"witness","eid":"BJq7UABlttINuWJh1Xl2lkqZG4NTdUdqnbFJDa6ZyxCC"}';
     print(issuer_oobi);
 
-    var issuer_oobis = "${witness_oobi[0]}$issuer_oobi";
-    print("rrr: ${issuer_oobis}");
+    var st = await api.getCredentialState(
+        identifier: verifyingIdentifier, credentialSaid: credential.vcId);
+    expect(st, null);
 
-    try {
-      await api.verifyFromCesr(stream: signed);
-    } catch (e) {
-      expect(e.toString().contains("Verification failed"), true);
-    }
-    ;
-
-    // var result = ;
-    // print(result);
-
+    await api.sendOobiToWatcher(
+        identifier: verifyingIdentifier, oobisJson: witnessOobi[0]);
     await api.sendOobiToWatcher(
         identifier: verifyingIdentifier, oobisJson: issuer_oobi);
     var query = await api.queryWatchers(
-        whoAsk: verifyingIdentifier, aboutWho: signing_identifier);
+        whoAsk: verifyingIdentifier, aboutWho: signingIdentifier);
 
     for (final qry in query) {
-      var raw_signature = await algorithm.sign(
+      var rawSignature = await algorithm.sign(
         utf8.encode(qry),
         keyPair: verifierKeyPair0,
       );
-      var hex_signature = await api.signatureFromHex(
+      var hexSignature = await api.signatureFromHex(
           st: SignatureType.Ed25519Sha512,
-          signature: hex.encode(raw_signature.bytes));
+          signature: hex.encode(rawSignature.bytes));
       await api.finalizeQuery(
           identifier: verifyingIdentifier,
           queryEvent: qry,
-          signature: hex_signature);
+          signature: hexSignature);
     }
 
-    var result = await api.verifyFromCesr(stream: signed);
-    print(result);
+    var telQry = await api.queryTel(
+        identifier: verifyingIdentifier,
+        registryId: ixn.registryId,
+        credentialSaid: credential.vcId);
+
+    var telRawSignature = await algorithm.sign(
+      utf8.encode(telQry),
+      keyPair: verifierKeyPair0,
+    );
+    var telHexSignature = await api.signatureFromHex(
+        st: SignatureType.Ed25519Sha512,
+        signature: hex.encode(telRawSignature.bytes));
+    await api.finalizeTelQuery(
+        identifier: verifyingIdentifier,
+        queryEvent: telQry,
+        signature: telHexSignature);
+
+    st = await api.getCredentialState(
+        identifier: verifyingIdentifier, credentialSaid: credential.vcId);
+    expect(st?.contains("Issued"), true);
   });
 }
