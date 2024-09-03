9f90fa2e967fa6ecf0bdb6eff707f0aadd3330fa
Edyta Pawlak <edyta.pawlakk@gmail.com>
2024-09-03 12:03:04 +0200

    chore(node): remove unused code

--- bindings/node.js/test/support/b64.ts
+++
@@ -1,5 +0,0 @@
-import URLSafeBase64 from 'urlsafe-base64';
-
-let b64EncodeUrlSafe = (p: Uint8Array) => URLSafeBase64.encode(Buffer.from(p));
-
-export { b64EncodeUrlSafe };
--- bindings/node.js/test/support/kel.ts
+++
@@ -1,3 +0,0 @@
-let countEvents = (KEL: string) => KEL.match(/\{((?:[^{}]*\{[^{}]*\})*[^{}]*?)\}/g).length;
-
-export { countEvents };
